02d40d2e2924319f2538fbf15f90141fb05b9346
TerenceTT <terence.timbang@snowflake.com>
2023-04-14 20:55:50 +0800

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,8 @@ if __name__ == "__main__":
     st.title("Tasty Bytes: Visualizing Geospatial Data")
     session = create_session_object()
 
+    #Section 1 - Top 10 sales locations
+
     #Query for top 10 locations
     df = pd.DataFrame(session.sql("SELECT TOP 10 o.location_id, o.location_name, o.longitude, o.latitude, SUM(o.price) AS total_sales_usd FROM frostbyte_tasty_bytes.analytics.orders_v o WHERE 1=1 AND o.primary_city = 'Paris' AND YEAR(o.date) = 2022 GROUP BY 1,2,3,4 ORDER BY total_sales_usd DESC").to_pandas())
     
@@ -46,6 +48,9 @@ if __name__ == "__main__":
         folium.Marker(location=[row['LATITUDE'],row['LONGITUDE']],
                     popup = popup, c=row['LOCATION_NAME']).add_to(m)
     
+    #Adjust map zoom
+    m.fit_bounds(m.get_bounds(), padding=(30, 30))
+
     # Draw the map
     st.subheader("Map View:")
     st_data = folium_static(m, width=700)
@@ -68,7 +73,7 @@ if __name__ == "__main__":
     st.subheader("Map View:")
     st_data = folium_static(m, width=700)
 
-    # Section 2 - Finding our Top Selling Locations Center Point
+    # Section 3 - Finding our Top Selling Locations Center Point
     st.header("Top Selling Locations Center Point")
     df2 = pd.DataFrame(session.sql("WITH _top_10_locations AS ( SELECT TOP 10 o.location_id, ST_MAKEPOINT(o.longitude, o.latitude) AS geo_point, SUM(o.price) AS total_sales_usd FROM frostbyte_tasty_bytes.analytics.orders_v o WHERE 1=1 AND o.primary_city = 'Paris' AND YEAR(o.date) = 2022 GROUP BY o.location_id, o.latitude, o.longitude ORDER BY total_sales_usd DESC ) SELECT ST_COLLECT(tl.geo_point) AS collect_points, ST_CENTROID(collect_points) AS geometric_center_point FROM _top_10_locations tl;").to_pandas())
     st.subheader("Raw Data:")
@@ -85,7 +90,7 @@ if __name__ == "__main__":
     st.subheader("Map View:")
     st_data = folium_static(m, width=700)
 
-    # Section 3 - Farthest Locations from top selling center point
+    # Section 4 - Farthest Locations from top selling center point
     st.header("Farthest Locations From Top Selling Center Point")
     st.text('set center point')
     center_point = { "coordinates": [ 2.364853294993676e+00, 4.885681511418426e+01 ], "type": "Point" }
@@ -93,8 +98,6 @@ if __name__ == "__main__":
     df3 = pd.DataFrame(session.sql("WITH _2022_paris_locations AS ( SELECT DISTINCT o.location_id, o.location_name, o.longitude, o.latitude, ST_MAKEPOINT(o.longitude, o.latitude) AS geo_point FROM frostbyte_tasty_bytes.analytics.orders_v o WHERE 1=1 AND o.primary_city = 'Paris' AND YEAR(o.date) = 2022 ) SELECT TOP 50 ll.location_id, ll.location_name, ll.longitude, ll.latitude, ROUND(ST_DISTANCE(ll.geo_point, TO_GEOGRAPHY(" + str(center_point) + "))/1000,2) AS kilometer_from_top_selling_center FROM _2022_paris_locations ll ORDER BY kilometer_from_top_selling_center DESC;").to_pandas())
     st.subheader("Raw Data:")
     st.table(df3)
-
-    m.fit_bounds(m.get_bounds(), padding=(30, 30))
 
     #Loop through each row in the dataframe
     for i,row in df3.iterrows():
@@ -108,6 +111,9 @@ if __name__ == "__main__":
         folium.Marker(location=[row['LATITUDE'],row['LONGITUDE']],
                     popup = popup, c=row['LOCATION_NAME'],icon=folium.Icon(color="red", icon="triangle-exclamation")).add_to(m)
     
+    #Adjust map zoom
+    m.fit_bounds(m.get_bounds(), padding=(30, 30))
+
     ## Draw the map
     st.subheader("Map View:")
     st_data = folium_static(m, width=700)
