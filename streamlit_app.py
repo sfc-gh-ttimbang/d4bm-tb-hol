9f291369af033aceea00c69495773ca79f66b267
TerenceTT <terence.timbang@snowflake.com>
2023-04-14 15:09:37 +0800

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,38 +50,21 @@ if __name__ == "__main__":
     st.subheader("Map View:")
     st_data = folium_static(m, width=700)
 
+
+
+    # Section 2 - Bounding Area
     st.header("Minimum Bounding Area")
     df1 = pd.DataFrame(session.sql("WITH _top_10_locations AS ( SELECT TOP 10 o.location_id, ST_MAKEPOINT(o.longitude, o.latitude) AS geo_point, SUM(o.price) AS total_sales_usd FROM frostbyte_tasty_bytes.analytics.orders_v o WHERE 1=1 AND o.primary_city = 'Paris' AND YEAR(o.date) = 2022 GROUP BY o.location_id, o.latitude, o.longitude ORDER BY total_sales_usd DESC ) SELECT ST_NPOINTS(ST_COLLECT(tl.geo_point)) AS count_points_in_collection, ST_COLLECT(tl.geo_point) AS collection_of_points, ST_ENVELOPE(collection_of_points) AS minimum_bounding_polygon, ROUND(ST_AREA(minimum_bounding_polygon)/1000000,2) AS area_in_sq_kilometers FROM _top_10_locations tl").to_pandas())
     st.subheader("Raw Data:")
     st.table(df1)
-
-    #temp1 = df1['MINIMUM_BOUNDING_POLYGON'][0]
-    #df1a = pd.read_json(temp1) 
     df1a = pd.read_json(df1['MINIMUM_BOUNDING_POLYGON'][0])
-    st.table(df1a)
-    st.text('["coordinates"][0]')
-    st.text(df1a["coordinates"][0])
-
-    st.text('["coordinates"][0][0]')
-    st.text(df1a["coordinates"][0][0])
 
     st.text('Iterating')
     bounding_coords = list()
     for i in df1a["coordinates"][0]:
-        bounding_coords.append((i[1],i[0]))   
-        st.text('item:')
-        st.text(i)
-        st.text('contents')
-        st.text(i[0])
-        st.text(i[1])
+        bounding_coords.append((i[1],i[0]))
     
     st.text(bounding_coords)
-
-    #coorlist = str(df1a["coordinates"][0])
-    #temp2 = coorlist.replace('[','(')
-    #temp3 = temp2.replace(']',')')
-    #bounding_coords = temp3[1:-1]
-    #st.text(bounding_coords)
 
     folium.PolyLine(bounding_coords, tooltip="Minimum Bounding Area").add_to(m)
 
