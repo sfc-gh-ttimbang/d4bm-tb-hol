7043b47de562991b6cba38edd024faf69f14b0ec
TerenceTT <terence.timbang@snowflake.com>
2023-04-14 14:50:13 +0800

    tes

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,8 @@ if __name__ == "__main__":
         st.text('item:')
         st.text(i)
         st.text('contents')
-        st.text(i[0]+','+i[1])
+        st.text(i[0])
+        st.text(i[1])
 
 
 
